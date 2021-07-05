c22c66c7d0c3f40d443818a09e3dd4f9a1a013db
arnaud290 <arnaud.manach@gmail.com>
2021-07-05 12:49:16 +0200

    sentry OK

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,11 +1,20 @@
 import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk import capture_exception
 
+SENTRY_DSN=os.environ.get('SENTRY_DSN')
 sentry_sdk.init(
-    dsn=os.environ.get('SENTRY_DSN'),
-    integrations=[DjangoIntegration()]
+    dsn=SENTRY_DSN,
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
 )
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
--- urls.py
+++ urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path(
         '',
@@ -10,4 +13,5 @@ urlpatterns = [
     path('lettings/', include(('lettings.urls', 'lettings'), namespace='lettings')),
     path('profiles/', include(('profiles.urls', 'profiles'), namespace='profiles')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error)
 ]
